c5e17b287bc48dd4c1c3f4490bc454f6cbd3d14c
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-19 23:11:25 -0700

    cleaned up app.js header

--- src/App.js
+++ src/App.js
@@ -22,10 +22,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Markdown>{this.state.loadedFile}</Markdown>
       </div>
     );
