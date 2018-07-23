b92489bef70b41411506eae5b30b0ae9d03d8e01
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-22 19:57:29 -0700

    added header styledcomponent for a styled header on main process

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header>Journaler</Header>
         <Split>
           <CodeWindow>
             <AceEditor
@@ -49,6 +50,22 @@ class App extends Component {
 }
 
 export default App;
+
+const Header = styled.header`
+  background-color: #191324;
+  color: #75717c;
+  font-size: 0.8rem;
+  height: 23px;
+  text-align: center;
+  position: fixed;
+  padding-top: 3px;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.2);
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  -webkit-app-region: drag;
+`;
 
 const Split = styled.div`
   display: flex;
