020de542a419c605c2c00b62c1575d5ddbbfe6ac
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-27 19:44:18 -0700

    removed electron-react-devtools from main process due to error

--- src/main.js
+++ src/main.js
@@ -17,7 +17,6 @@ function createWindow() {
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000');
   require('devtron').install();
-  require('electron-react-devtools').install();
   //Create Menu
   const template = [
     {
